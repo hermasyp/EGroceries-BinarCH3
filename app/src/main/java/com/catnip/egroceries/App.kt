6b312f19037a2d7ba592e527f00b64bcb1f3ce2b
Muhammad Hermas Yuda Pamungkas <hermas.yuda@gmail.com>
2023-11-06 12:48:22 +0700

    reformat code and install ktlint

--- app/src/main/java/com/catnip/egroceries/App.kt
+++ app/src/main/java/com/catnip/egroceries/App.kt
@@ -1,27 +1,12 @@
 package com.catnip.egroceries
 
 import android.app.Application
-import com.catnip.egroceries.di.AppModules
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.GlobalContext.startKoin
+import dagger.hilt.android.HiltAndroidApp
 
 /**
 Written with love by Muhammad Hermas Yuda Pamungkas
 Github : https://github.com/hermasyp
  **/
 
-class App : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        initKoin()
-    }
-
-    private fun initKoin() {
-        startKoin {
-            androidLogger()
-            androidContext(this@App)
-            modules(AppModules.modules)
-        }
-    }
-}
+@HiltAndroidApp
+class App : Application()
